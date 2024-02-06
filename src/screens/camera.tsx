75250f798c0dbff6e04f0fd6593a805c029d580e
Legoracer <maxivanec5@gmail.com>
2024-02-06 11:43:29 +0100

    display

--- src/screens/camera.tsx
+++ src/screens/camera.tsx
@@ -1,26 +1,50 @@
-import { View, Text } from "react-native"
-import { Camera, useCameraDevice, useCodeScanner } from "react-native-vision-camera";
+import { useEffect, useState } from "react";
+import { View, Text, StyleSheet } from "react-native"
+import { Camera, useCameraDevice, useCodeScanner, CodeScanner } from "react-native-vision-camera";
+
+let style = StyleSheet.create({
+    fullscreen: {
+        flex: 1,
+        height: '100%',
+        width: '100%',
+        flexDirection: 'row',
+        alignItems: 'flex-end',
+        justifyContent: 'center'
+    },
+
+    block: {
+        backgroundColor: 'rgba(255, 255, 255, 0.5)',
+        borderColor: 'black',
+        color: 'white',
+        marginBottom: 50,
+        padding: 10,
+        borderRadius: 1,
+        fontSize: 20
+    }
+})
 
 export default function SplashScreen() {
     let device = useCameraDevice("back")
+    let [qr, setQR] = useState("")
 
     const codeScanner = useCodeScanner({
         codeTypes: ['qr', 'ean-13'],
         onCodeScanned: (codes) => {
-            console.log("########")
-            codes.forEach((v, i) => {
-                console.log(i, v.value)
-            })
-        }
+            setQR(codes[0].value)
+        },
     })
 
     return (
-        <View>
+        <View style={style.fullscreen}>
             <Camera
+                style={style.fullscreen}
                 isActive={true}
                 device={device}
                 codeScanner={codeScanner}
-            ></Camera>
+            >
+                <Text style={style.block}>{qr}</Text>
+            </Camera>
         </View>
     );
 }
+
